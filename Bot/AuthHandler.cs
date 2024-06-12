beffa42da0c88fe62b4efc433800a6d9de709149
Kodytek Jakub <jkodytek@retia.cz>
2024-06-12 11:39:46 +0200

    ADD bot authentication

---
+++ Bot/AuthHandler.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+
+
+namespace discord_bot.Bot
+{
+    internal class AuthHandler
+    {
+        readonly static ConfigurationBuilder builder = new();
+        private static string? token;
+
+        public static void Authenticate() 
+        {
+            builder.SetBasePath(AppContext.BaseDirectory).AddJsonFile("appsettings.json");
+            LoadToken();
+        }
+
+        private static void LoadToken()
+        {
+            token = GetToken();
+            if (token == null)
+            {
+                Console.WriteLine("Token not found in appsettings.json");
+                Environment.Exit(1);
+            }
+            Console.WriteLine("Token loaded successfully");
+        }
+
+        private static string? GetToken()
+        {
+            IConfiguration config = builder.Build();
+            return config["Token"];
+        }
+    }
+}
---
+++ Bot/Utility/Logger.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace discord_bot.Bot.Utility
+{
+    internal class Logger
+    {
+        static Logger()
+        {
+        }
+
+        public static void Introduce(string channel) 
+        {
+        
+        }
+
+        public static void WriteLog(string message)
+        {
+            Console.WriteLine($"{DateTime.Now}: {message}");
+        }
+        public static void WriteLog(string message, string user)
+        {
+            Console.WriteLine($"{DateTime.Now}: {user} - {message}");
+        }
+
+        public static void WriteErrorLog(string message)
+        {
+            Console.Error.WriteLine($"{DateTime.Now}: {message}");
+        }
+
+        public static void WriteErrorLog(string message, string user)
+        {
+            Console.Error.WriteLine($"{DateTime.Now}: {user} - {message}");
+        }
+
+    }
+}
